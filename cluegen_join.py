b864071e39f0f90da61fcc60dfa7854f24ef363e
Arthur Henrique <arthur393@gmail.com>
2020-05-13 09:50:33 -0300

    Change '+' string operator to .join()
    In order to see the difference of performance when runs generated code.
    based:
    https://towardsdatascience.com/do-not-use-to-join-strings-in-python-f89908307273
    
    perf results:
    cluegen 0.15228939056396484
    cluegen_eval 1.567594051361084
    cluegen_join 0.14189982414245605
    cluegen_join_eval 2.3097891807556152
    
    -------------------------------------
    cluegen 0.130767822265625
    cluegen_eval 1.5651187896728516
    cluegen_join 0.1430985927581787
    cluegen_join_eval 2.6827592849731445
    
    -------------------------------------
    cluegen 0.1327824592590332
    cluegen_eval 1.7096567153930664
    cluegen_join 0.13564133644104004
    cluegen_join_eval 2.7156412601470947
    
    -------------------------------------
    cluegen 0.13405179977416992
    cluegen_eval 1.5626211166381836
    cluegen_join 0.14440274238586426
    cluegen_join_eval 2.8520944118499756
    
    -------------------------------------
    cluegen 0.1323540210723877
    cluegen_eval 1.5628275871276855
    cluegen_join 0.14211750030517578
    cluegen_join_eval 2.3693506717681885
    
    -------------------------------------
    cluegen 0.1311657428741455
    cluegen_eval 1.5744280815124512
    cluegen_join 0.14488482475280762
    cluegen_join_eval 2.6973989009857178
    
    -------------------------------------
    cluegen 0.13548684120178223
    cluegen_eval 1.5867490768432617
    cluegen_join 0.1759488582611084
    cluegen_join_eval 2.7186837196350098

---
+++ cluegen_join.py
@@ -0,0 +1,123 @@
+# cluegen_join.py
+#
+# Classes generated from type clues.
+#
+#     https://github.com/dabeaz/cluegen
+#
+# Author: David Beazley (@dabeaz).
+#         http://www.dabeaz.com
+#
+# Copyright (C) 2018-2020.
+#
+# Permission is granted to use, copy, and modify this code in any
+# manner as long as this copyright message and disclaimer remain in
+# the source code.  There is no warranty.  Try to use the code for the
+# greater good.
+
+import types
+
+# Collect all type clues from a class and base classes.
+def all_clues(cls):
+    clues = {}
+    for c in reversed(cls.__mro__):
+        clues.update(getattr(c, "__annotations__", {}))
+    return clues
+
+
+# Decorator to define methods of a class as a code generator.
+def cluegen(func):
+    def __get__(self, instance, cls):
+        locs = {}
+        code = func(cls)
+        exec(code, locs)
+        meth = locs[func.__name__]
+        setattr(cls, func.__name__, meth)
+        return meth.__get__(instance, cls)
+
+    def __set_name__(self, cls, name):
+        methods = cls.__dict__.get("_methods", list(cls._methods))
+        if "_methods" not in cls.__dict__:
+            cls._methods = methods
+        cls._methods.append((name, self))
+
+    return type(
+        f"ClueGen_{func.__name__}", (), dict(__get__=__get__, __set_name__=__set_name__)
+    )()
+
+
+# Base class for defining data structures
+class DatumBase:
+    __slots__ = ()
+    _methods = []
+
+    @classmethod
+    def __init_subclass__(cls):
+        submethods = []
+        for name, val in cls._methods:
+            if name not in cls.__dict__:
+                setattr(cls, name, val)
+                submethods.append((name, val))
+            elif val is cls.__dict__[name]:
+                submethods.append((name, val))
+
+        if submethods != cls._methods:
+            cls._methods = submethods
+
+
+class Datum(DatumBase):
+    __slots__ = ()
+
+    @cluegen
+    def __init__(cls):
+        clues = all_clues(cls)
+        args = ", ".join(
+            f"{name}={getattr(cls,name)!r}"
+            if hasattr(cls, name)
+            and not isinstance(getattr(cls, name), types.MemberDescriptorType)
+            else name
+            for name in clues
+        )
+        body = "\n".join(f"   self.{name} = {name}" for name in clues)
+        return f"def __init__(self, {args}):\n{body}\n"
+
+    @cluegen
+    def __repr__(cls):
+        clues = all_clues(cls)
+        fmt = ", ".join("%s={self.%s!r}" % (name, name) for name in clues)
+        return "def __repr__(self):\n" '    return f"{type(self).__name__}(%s)"' % fmt
+
+    @cluegen
+    def __iter__(cls):
+        clues = all_clues(cls)
+        values = "\n".join(f"   yield self.{name}" for name in clues)
+        return "".join(["def __iter__(self):\n", values])
+
+    @cluegen
+    def __eq__(cls):
+        clues = all_clues(cls)
+        selfvals = ",".join(f"self.{name}" for name in clues)
+        othervals = ",".join(f"other.{name}" for name in clues)
+        return (
+            "def __eq__(self, other):\n"
+            "    if self.__class__ is other.__class__:\n"
+            f"        return ({selfvals},) == ({othervals},)\n"
+            "    else:\n"
+            "        return NotImplemented\n"
+        )
+
+    @cluegen
+    def __hash__(cls):
+        clues = all_clues(cls)
+        if clues:
+            self_tuple = "".join(["(", ",".join(f"self.{name}" for name in clues), ")"])
+        else:
+            self_tuple = "()"
+        return "def __hash__(self):\n" f"    return hash({self_tuple})\n"
+
+
+# Example use
+if __name__ == "__main__":
+    # Start defining classes
+    class Coordinates(Datum):
+        x: int
+        y: int
--- perf.py
+++ perf.py
@@ -3,7 +3,7 @@
 import sys
 import time
 
-standard_template = '''
+standard_template = """
 class C{n}:
     def __init__(self, a, b, c, d, e):
         self.a = a
@@ -20,13 +20,13 @@ class C{n}:
             return (self.a, self.b, self.c, self.d, self.e) == (other.a, other.b, other.c, other.d, other.e)
         else:
             return NotImplemented
-'''
+"""
 
-namedtuple_template = '''
+namedtuple_template = """
 C{n} = namedtuple('C{n}', ['a', 'b', 'c', 'd', 'e'])
-'''
+"""
 
-dataclass_template = '''
+dataclass_template = """
 @dataclass
 class C{n}:
     a : int
@@ -34,9 +34,9 @@ class C{n}:
     c : int
     d : int
     e : int
-'''
+"""
 
-attr_template = '''
+attr_template = """
 @attr.s
 class C{n}:
     a = attr.ib()
@@ -44,19 +44,19 @@ class C{n}:
     c = attr.ib()
     d = attr.ib()
     e = attr.ib()
-'''
+"""
 
-cluegen_template = '''
+cluegen_template = """
 class C{n}(Datum):
     a : int
     b : int
     c : int
     d : int
     e : int
-'''
+"""
 
 # cluegen, but same default methods as dataclasses generated
-cluegen_eval_template = '''
+cluegen_eval_template = """
 class C{n}(Datum):
     a : int
     b : int
@@ -65,45 +65,56 @@ class C{n}(Datum):
     e : int
 
 C{n}.__init__, C{n}.__repr__, C{n}.__eq__
-'''
+"""
+
 
 def run_test(name, n):
     start = time.time()
     while n > 0:
         import perftemp
-        del sys.modules['perftemp']
+
+        del sys.modules["perftemp"]
         n -= 1
     end = time.time()
-    print(name, (end-start))
+    print(name, (end - start))
+
 
 def write_perftemp(count, template, setup):
-    with open('perftemp.py', 'w') as f:
+    with open("perftemp.py", "w") as f:
         f.write(setup)
         for n in range(count):
             f.write(template.format(n=n))
-    
+
+
 def main(reps):
-    write_perftemp(100, standard_template, '')
-    run_test('standard classes', reps)
+    # write_perftemp(100, standard_template, "")
+    # run_test("standard classes", reps)
 
-    write_perftemp(100, namedtuple_template, 'from collections import namedtuple\n')
-    run_test('namedtuple', reps)
+    # write_perftemp(100, namedtuple_template, "from collections import namedtuple\n")
+    # run_test("namedtuple", reps)
 
-    write_perftemp(100, dataclass_template, 'from dataclasses import dataclass\n')
-    run_test('dataclasses', reps)
-    try:
-        write_perftemp(100, attr_template, 'import attr\n')
-        run_test('attrs', reps)
-    except ImportError:
-        print("attrs not installed")
+    # write_perftemp(100, dataclass_template, "from dataclasses import dataclass\n")
+    # run_test("dataclasses", reps)
+    # try:
+    #     write_perftemp(100, attr_template, "import attr\n")
+    #     run_test("attrs", reps)
+    # except ImportError:
+    #     print("attrs not installed")
 
-    write_perftemp(100, cluegen_template, 'from cluegen import Datum\n')
-    run_test('cluegen', reps)
+    write_perftemp(100, cluegen_template, "from cluegen import Datum\n")
+    run_test("cluegen", reps)
 
-    write_perftemp(100, cluegen_eval_template, 'from cluegen import Datum\n')
-    run_test('cluegen_eval', reps)
+    write_perftemp(100, cluegen_eval_template, "from cluegen import Datum\n")
+    run_test("cluegen_eval", reps)
 
-if __name__ == '__main__':
+    write_perftemp(100, cluegen_template, "from cluegen_join import Datum\n")
+    run_test("cluegen_join", reps)
+
+    write_perftemp(100, cluegen_eval_template, "from cluegen_join import Datum\n")
+    run_test("cluegen_join_eval", reps)
+
+
+if __name__ == "__main__":
     if len(sys.argv) == 2:
         reps = int(sys.argv[1])
     else:
